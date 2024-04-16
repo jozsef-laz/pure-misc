679f8cda0529afc51ce016f860ab12844288af9b
Jozsef Laz <jlaz@purestorage.com>
2024-04-16 10:19:08 +0200

    collect.sh: option for last <n> logfiles + fixing SSH_PARAMS

--- collect.sh
+++ collect.sh
@@ -1,27 +1,29 @@
 #!/usr/bin/env bash
 
 LOGTYPES_DEFAULT_ARG="middleware,platform,nfs"
+DEFAULT_NUMBER_OF_LOGFILES=3
 
 usage() {
    echo "Usage: $0 -c <clusters> [-h]" 1>&2
    echo "  -c <clusters>   where clusters are names of clusters separated by comma (ex. \"irp871-c01,irp871-c02\")" 1>&2
-   echo "  -l <logtypes>   list of logtypes which we want to download (separated by comma)"
-   echo "                     possible values:"
-   echo "                      - middleware: middleware.log"
-   echo "                      - platform: platform.log of FMs"
-   echo "                      - platform_blades: platform.log of blades"
-   echo "                      - nfs: nfs.log of blades"
-   echo "                      - system: system.log of FMs"
-   echo "                      - system_blades: system.log of blades"
-   echo "                     default value: \"$LOGTYPES_DEFAULT_ARG\""
+   echo "  -l <logtypes>   list of logtypes which we want to download (separated by comma)" 1>&2
+   echo "                     possible values:" 1>&2
+   echo "                      - middleware: middleware.log" 1>&2
+   echo "                      - platform: platform.log of FMs" 1>&2
+   echo "                      - platform_blades: platform.log of blades" 1>&2
+   echo "                      - nfs: nfs.log of blades" 1>&2
+   echo "                      - system: system.log of FMs" 1>&2
+   echo "                      - system_blades: system.log of blades" 1>&2
+   echo "                     default value: \"$LOGTYPES_DEFAULT_ARG\"" 1>&2
    echo "  -d <dir path>   relative path where log pack directory shall be created" 1>&2
    echo "                     (ex. \"path/to/downloads\", then logs will be under \"path/to/downloads/irpXXX-cXX_2024-01-30-T17-00-37)" 1>&2
    echo "                     only irpXXX-cXX... directory will be created but no parents (for safety considerations)" 1>&2
+   echo "  -n <number>     collect only the last <number> of logfiles, default value: $DEFAULT_NUMBER_OF_LOGFILES" 1>&2
    echo "  -h              help" 1>&2
    exit 1
 }
 
-while getopts "c:l:d:h" o; do
+while getopts "c:l:d:n:h" o; do
    case "${o}" in
       c)
          CLUSTERS_ARG=${OPTARG}
@@ -31,6 +33,9 @@ while getopts "c:l:d:h" o; do
          ;;
       d)
          DIR_PREFIX=${OPTARG}
+         ;;
+      n)
+         NUMBER_OF_LOGFILES=${OPTARG}
          ;;
       *)
          usage
@@ -53,7 +58,11 @@ echo
 if [ -z "${LOGTYPES_ARG}" ]; then
    LOGTYPES_ARG=$LOGTYPES_DEFAULT_ARG
 fi
+if [ -z "${NUMBER_OF_LOGFILES}" ]; then
+   NUMBER_OF_LOGFILES=$DEFAULT_NUMBER_OF_LOGFILES
+fi
 echo "logtypes to download: [$LOGTYPES_ARG]"
+echo "number of each logfile type: [$NUMBER_OF_LOGFILES]"
 DOWNLOAD_MIDDLEWARE_LOG=$(echo "$LOGTYPES_ARG" | grep "middleware")
 DOWNLOAD_PLATFORM_LOG=$(echo "$LOGTYPES_ARG" | grep "platform")
 DOWNLOAD_PLATFORM_BLADES_LOG=$(echo "$LOGTYPES_ARG" | grep "platform_blades")
@@ -72,9 +81,9 @@ DOWNLOAD_SYSTEM_BLADES_LOG=$(echo "$LOGTYPES_ARG" | grep "system_blades")
 
 # TODO: use this
 SSH_PARAMS="\
-   -o \"UserKnownHostsFile=/dev/null\" \
-   -o \"StrictHostKeyChecking=no\" \
-   -o \"LogLevel=ERROR\""
+   -o UserKnownHostsFile=/dev/null \
+   -o StrictHostKeyChecking=no \
+   -o LogLevel=ERROR"
 
 DATE=$(date "+%F-T%H-%M-%S")
 # directory in which we download the logs
@@ -96,10 +105,7 @@ download_file() {
    REMOTE_FILEPATH=$1
    LOCAL_FILEPATH=$2
    echo -n "Downloading: $REMOTE_FILEPATH ..."
-   sshpass -p welcome scp \
-      -o "UserKnownHostsFile=/dev/null" \
-      -o "StrictHostKeyChecking=no" \
-      -o LogLevel=ERROR \
+   sshpass -p welcome scp $SSH_PARAMS \
       $REMOTE_FILEPATH $LOCAL_FILEPATH
    RETVAL=$?
    if [ $RETVAL != 0 ]; then
@@ -124,29 +130,21 @@ download_file_from_blade() {
    # already login to the blade
 
    # first copy the file from blade to FM, and then from FM to local machine
-   sshpass -p welcome ssh \
-      -o "UserKnownHostsFile=/dev/null" \
-      -o "StrictHostKeyChecking=no" \
-      -o LogLevel=ERROR \
-      ir@$FM_IP "rm -f $LOGFILE; scp ir$BLADENUM:/logs/$LOGFILE ."
+   sshpass -p welcome ssh $SSH_PARAMS \
+      ir@$FM_IP \
+      "rm -f $LOGFILE; scp ir$BLADENUM:/logs/$LOGFILE ."
    RETVAL=$?
    if [ $RETVAL != 0 ]; then
       echo " Error in scp #1: RETVAL=$RETVAL"
    fi
-   sshpass -p welcome scp \
-      -o "UserKnownHostsFile=/dev/null" \
-      -o "StrictHostKeyChecking=no" \
-      -o LogLevel=ERROR \
+   sshpass -p welcome scp $SSH_PARAMS \
       ir@$FM_IP:/home/ir/$LOGFILE $BLADE_DIR
    RETVAL=$?
    if [ $RETVAL != 0 ]; then
       echo " Error in scp #2: RETVAL=$RETVAL"
    fi
    # cleanup
-   sshpass -p welcome ssh \
-      -o "UserKnownHostsFile=/dev/null" \
-      -o "StrictHostKeyChecking=no" \
-      -o LogLevel=ERROR \
+   sshpass -p welcome ssh $SSH_PARAMS \
       ir@$FM_IP "rm -f $LOGFILE"
    RETVAL=$?
    if [ $RETVAL != 0 ]; then
@@ -163,43 +161,31 @@ for CLUSTER in $CLUSTERS; do
    for FMNUM in ${FMNUM_LIST[@]}; do
       echo "collecting from FM #$FMNUM"
       # getting admin ip of FM
-      FM_IP=$(sshpass -p welcome ssh \
-         -o "UserKnownHostsFile=/dev/null" \
-         -o "StrictHostKeyChecking=no" \
-         -o LogLevel=ERROR \
+      FM_IP=$(sshpass -p welcome ssh $SSH_PARAMS \
          ir@$CLUSTER "purenetwork list --csv | grep 'fm$FMNUM.admin0,' | cut -d',' -f4")
       FM_DIR=$DIR/${CLUSTER}_sup${FMNUM}_${FM_IP}
       echo "FM_IP = [$FM_IP], FM_DIR=[$FM_DIR]"
       mkdir $FM_DIR
       # TODO: put these in function
       if [ ! -z "$DOWNLOAD_MIDDLEWARE_LOG" ]; then
-         LOGFILES=$(sshpass -p welcome ssh \
-            -o "UserKnownHostsFile=/dev/null" \
-            -o "StrictHostKeyChecking=no" \
-            -o LogLevel=ERROR \
-            ir@$FM_IP "cd /logs; ls middleware.log*")
+         LOGFILES=$(sshpass -p welcome ssh $SSH_PARAMS \
+            ir@$FM_IP "cd /logs; ls middleware.log* -tr | tail --lines $NUMBER_OF_LOGFILES")
          echo "LOGFILES=[$LOGFILES]"
          for LOGFILE in $LOGFILES; do
             download_file ir@$FM_IP:/logs/$LOGFILE $FM_DIR
          done
       fi
       if [ ! -z "$DOWNLOAD_PLATFORM_LOG" ]; then
-         LOGFILES=$(sshpass -p welcome ssh \
-            -o "UserKnownHostsFile=/dev/null" \
-            -o "StrictHostKeyChecking=no" \
-            -o LogLevel=ERROR \
-            ir@$FM_IP "cd /logs; ls platform.log*")
+         LOGFILES=$(sshpass -p welcome ssh $SSH_PARAMS \
+            ir@$FM_IP "cd /logs; ls platform.log* -tr | tail --lines $NUMBER_OF_LOGFILES")
          echo "LOGFILES=[$LOGFILES]"
          for LOGFILE in $LOGFILES; do
             download_file ir@$FM_IP:/logs/$LOGFILE $FM_DIR
          done
       fi
       if [ ! -z "$DOWNLOAD_SYSTEM_LOG" ]; then
-         LOGFILES=$(sshpass -p welcome ssh \
-            -o "UserKnownHostsFile=/dev/null" \
-            -o "StrictHostKeyChecking=no" \
-            -o LogLevel=ERROR \
-            ir@$FM_IP "cd /logs; ls system.log*")
+         LOGFILES=$(sshpass -p welcome ssh $SSH_PARAMS \
+            ir@$FM_IP "cd /logs; ls system.log* -tr | tail --lines $NUMBER_OF_LOGFILES")
          echo "LOGFILES=[$LOGFILES]"
          for LOGFILE in $LOGFILES; do
             download_file ir@$FM_IP:/logs/$LOGFILE $FM_DIR
@@ -208,15 +194,9 @@ for CLUSTER in $CLUSTERS; do
    done
 
    # for transferring the logs, we use FM1, because usually the active one is FM2
-   FM1_IP=$(sshpass -p welcome ssh \
-      -o "UserKnownHostsFile=/dev/null" \
-      -o "StrictHostKeyChecking=no" \
-      -o LogLevel=ERROR \
+   FM1_IP=$(sshpass -p welcome ssh $SSH_PARAMS \
       ir@$CLUSTER "purenetwork list --csv | grep 'fm1.admin0,' | cut -d',' -f4")
-   BLADENUM=$(sshpass -p welcome ssh \
-      -o "UserKnownHostsFile=/dev/null" \
-      -o "StrictHostKeyChecking=no" \
-      -o LogLevel=ERROR \
+   BLADENUM=$(sshpass -p welcome ssh $SSH_PARAMS \
       ir@$CLUSTER "pureblade list | grep healthy | tail -n1 | cut -d' ' -f1 | cut -d'B' -f2")
    echo "Cluster has $BLADENUM blades"
    for NUM in $(seq 1 $BLADENUM); do
@@ -226,31 +206,22 @@ for CLUSTER in $CLUSTERS; do
 
       # TODO: future improvement could be to limit how many logs we download from a blade or for how much time in the past we're looking back
       if [ ! -z "$DOWNLOAD_NFS_LOG" ]; then
-         LOGFILES=$(sshpass -p welcome ssh \
-            -o "UserKnownHostsFile=/dev/null" \
-            -o "StrictHostKeyChecking=no" \
-            -o LogLevel=ERROR \
-            ir@$CLUSTER "ssh ir$NUM \"cd /logs; ls nfs.log nfs.log.*\"")
+         LOGFILES=$(sshpass -p welcome ssh $SSH_PARAMS \
+            ir@$CLUSTER "ssh ir$NUM \"cd /logs; ls nfs.log nfs.log.* -tr | tail --lines $NUMBER_OF_LOGFILES\"")
          for LOGFILE in $LOGFILES; do
             download_file_from_blade $LOGFILE $BLADE_DIR $NUM $FM1_IP $CLUSTER
          done
       fi
       if [ ! -z "$DOWNLOAD_PLATFORM_BLADES_LOG" ]; then
-         LOGFILES=$(sshpass -p welcome ssh \
-            -o "UserKnownHostsFile=/dev/null" \
-            -o "StrictHostKeyChecking=no" \
-            -o LogLevel=ERROR \
-            ir@$CLUSTER "ssh ir$NUM \"cd /logs; ls platform.log*\"")
+         LOGFILES=$(sshpass -p welcome ssh $SSH_PARAMS \
+            ir@$CLUSTER "ssh ir$NUM \"cd /logs; ls platform.log* -tr | tail --lines $NUMBER_OF_LOGFILES\"")
          for LOGFILE in $LOGFILES; do
             download_file_from_blade $LOGFILE $BLADE_DIR $NUM $FM1_IP $CLUSTER
          done
       fi
       if [ ! -z "$DOWNLOAD_SYSTEM_BLADES_LOG" ]; then
-         LOGFILES=$(sshpass -p welcome ssh \
-            -o "UserKnownHostsFile=/dev/null" \
-            -o "StrictHostKeyChecking=no" \
-            -o LogLevel=ERROR \
-            ir@$CLUSTER "ssh ir$NUM \"cd /logs; ls system.log*\"")
+         LOGFILES=$(sshpass -p welcome ssh $SSH_PARAMS \
+            ir@$CLUSTER "ssh ir$NUM \"cd /logs; ls system.log* -tr | tail --lines $NUMBER_OF_LOGFILES\"")
          for LOGFILE in $LOGFILES; do
             download_file_from_blade $LOGFILE $BLADE_DIR $NUM $FM1_IP $CLUSTER
          done
