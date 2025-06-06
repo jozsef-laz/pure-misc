f329adff3c35cbc8e3edf3bf72dd43d20a55e69a
Jozsef Laz <jlaz@purestorage.com>
2025-06-06 14:16:05 +0200

    collect.py: zstd decompress + ir_test.log copy + only run fm or blade stuff, when necessary

--- collect.py
+++ collect.py
@@ -10,6 +10,8 @@ warnings.filterwarnings(
 
 import os, io, paramiko
 import datetime
+import subprocess
+import shutil
 from paramiko.ssh_exception import SSHException
 from scp import SCPClient
 
@@ -77,8 +79,24 @@ parser = argparse.ArgumentParser(
                     prog='collect.py',
                     description='Collects logs from clusters')
 parser.add_argument('-c', '--clusters', type=str, help='where clusters are names of clusters separated by comma (ex. \"irp871-c01,irp871-c02\")')
-parser.add_argument('-l', '--logtypes', type=str, default=LOGTYPES_DEFAULT_ARG, help='help TODO')
-parser.add_argument('-d', '--dir-prefix', type=str, default='', help='help TODO')
+# TODO: nice format in help
+parser.add_argument('-l', '--logtypes', type=str, default=LOGTYPES_DEFAULT_ARG, help=f'''\
+list of logtypes which we want to download (separated by comma)" 1>&2
+   possible values:
+    - middleware: middleware.log
+    - platform: platform.log of FMs
+    - platform_blades: platform.log of blades
+    - nfs: nfs.log of blades
+    - system: system.log of FMs
+    - system_blades: system.log of blades
+    - haproxy_blades: haproxy.log of blades
+    - atop_blades: atop measurements on blades
+   default value: \"{LOGTYPES_DEFAULT_ARG}\"''')
+# TODO: nice format in help
+parser.add_argument('-d', '--dir-prefix', type=str, default='', help='''\
+relative path where log pack directory shall be created.
+(ex. \"path/to/downloads\", then logs will be under \"path/to/downloads/irpXXX-cXX_2024-01-30-T17-00-37)
+only irpXXX-cXX... directory will be created but no parents (for safety considerations)''')
 parser.add_argument('-n', '--num', type=int, default=DEFAULT_NUMBER_OF_LOGFILES, help=f'collect only the last <number> of logfiles, default value: {DEFAULT_NUMBER_OF_LOGFILES}')
 
 args = parser.parse_args()
@@ -94,6 +112,8 @@ assert len(clusters) > 0, 'len(clusters) is 0'
 logtypes=args.logtypes.split(',')
 print(f'logtypes = {logtypes}')
 assert len(logtypes) > 0, 'len(logtypes) is 0'
+want_blade_logs = list(set(logtypes) & {"nfs", "platform_blades", "system_blades", "haproxy_blades"})
+want_fm_logs = list(set(logtypes) & {"middleware", "platform", "system"})
 
 dir_prefix=args.dir_prefix
 print(f'dir_prefix = {dir_prefix}')
@@ -127,60 +147,74 @@ for cluster in clusters:
     assert ip1, 'ip1 is empty'
     assert ip2, 'ip2 is empty'
 
-    for ip in (ip1, ip2):
-        fm_num = 1 if ip is ip1 else 2
-        fm_dir = os.path.join(toplevel_logdir, f'{cluster}_sup{fm_num}_{ip}')
-        print(f'fm_num={fm_num}, ip={ip}, fm_dir={fm_dir}')
+    if want_fm_logs:
+        for ip in (ip1, ip2):
+            fm_num = 1 if ip is ip1 else 2
+            fm_dir = os.path.join(toplevel_logdir, f'{cluster}_sup{fm_num}_{ip}')
+            print(f'fm_num={fm_num}, ip={ip}, fm_dir={fm_dir}')
 
-        os.makedirs(fm_dir)
-        with agentNestedConnectWithPassword(None, ip, username="ir", password="welcome", look_for_keys=False) as client_fm:
-            logfiles = []
-            if 'middleware' in logtypes:
-                logfiles_str = run(client_fm, f'cd /logs; ls middleware.log* -tr | tail --lines {number_of_logfiles}')
-                logfiles += logfiles_str.split('\n')
-            if 'platform' in logtypes:
-                logfiles_str = run(client_fm, f'cd /logs; ls platform.log* -tr | tail --lines {number_of_logfiles}')
-                logfiles += logfiles_str.split('\n')
-            if 'system' in logtypes:
-                logfiles_str = run(client_fm, f'cd /logs; ls system.log* -tr | tail --lines {number_of_logfiles}')
-                logfiles += logfiles_str.split('\n')
-            print(f'logfiles = [{logfiles}]')
-            for logfile in logfiles:
-                scp = SCPClient(client_fm.get_transport())
-                download_file(scp, '/logs/'+logfile, fm_dir)
-
-
-    # standby_fm is indexed from 0!
-    standby_fm = 0 if master_fm == 2 else 1
-
-    # using standby_fm because it's less loaded
-    standby_ip = (ip1, ip2)[standby_fm]
-    with agentNestedConnectWithPassword(None, standby_ip, username="ir", password="welcome", look_for_keys=False) as client_fm:
-        bladelist_str = run(client_fm, "pureblade list --notitle | grep -v unused | cut -d' ' -f1 | cut -c7-")
-        bladelist = bladelist_str.split('\n')
-        assert len(bladelist) > 0, f'bladelist is empty, bladelist_str={bladelist_str}'
-        for bladenum in bladelist:
-            print(f'cluster={cluster}, blade=ir{bladenum}')
-            with nestedConnectWithKeyFromClient(client_fm, f'ir{bladenum}', username="ir", key=getKeyFromClient(client_fm, "/home/ir/.ssh/id_rsa")) as client_blade:
-                scp = SCPClient(client_blade.get_transport())
+            os.makedirs(fm_dir)
+            with agentNestedConnectWithPassword(None, ip, username="ir", password="welcome", look_for_keys=False) as client_fm:
                 logfiles = []
-                if 'nfs' in logtypes:
-                    logfiles_str = run(client_blade, f'cd /logs; ls nfs.log nfs.log.* -tr | tail --lines {number_of_logfiles}')
+                if 'middleware' in logtypes:
+                    logfiles_str = run(client_fm, f'cd /logs; ls middleware.log* -tr | tail --lines {number_of_logfiles}')
                     logfiles += logfiles_str.split('\n')
-                if 'platform_blade' in logtypes:
-                    logfiles_str = run(client_blade, f'cd /logs; ls platform.log* -tr | tail --lines {number_of_logfiles}')
+                if 'platform' in logtypes:
+                    logfiles_str = run(client_fm, f'cd /logs; ls platform.log* -tr | tail --lines {number_of_logfiles}')
                     logfiles += logfiles_str.split('\n')
-                if 'system_blade' in logtypes:
-                    logfiles_str = run(client_blade, f'cd /logs; ls system.log* -tr | tail --lines {number_of_logfiles}')
+                if 'system' in logtypes:
+                    logfiles_str = run(client_fm, f'cd /logs; ls system.log* -tr | tail --lines {number_of_logfiles}')
                     logfiles += logfiles_str.split('\n')
-                if 'haproxy_blade' in logtypes:
-                    logfiles_str = run(client_blade, f'cd /logs; ls haproxy.log* -tr | tail --lines {number_of_logfiles}')
-                    logfiles += logfiles_str.split('\n')
-                print(f'logfiles = [{logfiles}]')
-                local_blade_dir = os.path.join(toplevel_logdir, cluster, f'ir{bladenum}')
-                os.makedirs(local_blade_dir)
+                print(f'logfiles = {logfiles}')
                 for logfile in logfiles:
+                    scp = SCPClient(client_fm.get_transport())
+                    download_file(scp, '/logs/'+logfile, fm_dir)
+
+    if want_blade_logs:
+        # using standby_fm because it's less loaded
+        # standby_fm is indexed from 0!
+        standby_fm = 0 if master_fm == 2 else 1
+        standby_ip = (ip1, ip2)[standby_fm]
+        with agentNestedConnectWithPassword(None, standby_ip, username="ir", password="welcome", look_for_keys=False) as client_fm:
+            bladelist_str = run(client_fm, "pureblade list --notitle | grep -v unused | cut -d' ' -f1 | cut -c7-")
+            bladelist = bladelist_str.split('\n')
+            assert len(bladelist) > 0, f'bladelist is empty, bladelist_str={bladelist_str}'
+            for bladenum in bladelist:
+                print(f'cluster={cluster}, blade=ir{bladenum}')
+                with nestedConnectWithKeyFromClient(client_fm, f'ir{bladenum}', username="ir", key=getKeyFromClient(client_fm, "/home/ir/.ssh/id_rsa")) as client_blade:
                     scp = SCPClient(client_blade.get_transport())
-                    download_file(scp, '/logs/'+logfile, local_blade_dir)
+                    logfiles = []
+                    if 'nfs' in logtypes:
+                        logfiles_str = run(client_blade, f'cd /logs; ls nfs.log nfs.log.* -tr | tail --lines {number_of_logfiles}')
+                        logfiles += logfiles_str.split('\n')
+                    if 'platform_blade' in logtypes:
+                        logfiles_str = run(client_blade, f'cd /logs; ls platform.log* -tr | tail --lines {number_of_logfiles}')
+                        logfiles += logfiles_str.split('\n')
+                    if 'system_blade' in logtypes:
+                        logfiles_str = run(client_blade, f'cd /logs; ls system.log* -tr | tail --lines {number_of_logfiles}')
+                        logfiles += logfiles_str.split('\n')
+                    if 'haproxy_blade' in logtypes:
+                        logfiles_str = run(client_blade, f'cd /logs; ls haproxy.log* -tr | tail --lines {number_of_logfiles}')
+                        logfiles += logfiles_str.split('\n')
+                    print(f'logfiles = {logfiles}')
+                    local_blade_dir = os.path.join(toplevel_logdir, cluster, f'ir{bladenum}')
+                    os.makedirs(local_blade_dir)
+                    for logfile in logfiles:
+                        scp = SCPClient(client_blade.get_transport())
+                        download_file(scp, '/logs/'+logfile, local_blade_dir)
+
+print(f"decompressing every .zst file we downloaded in toplevel_logdir=[{toplevel_logdir}] ...")
+subprocess.run(["find", toplevel_logdir, "-name", "*.zst", "-exec", "sh", "-c",  'zstd -d "{}"; rm -f "{}"', ";"])
+
+current_time = datetime.datetime.now().timestamp()
+ir_test_log_time = os.path.getmtime('ir_test.log')
+if os.path.exists('ir_test.log') and ir_test_log_time < current_time:
+    ir_test_log_age = current_time - ir_test_log_time
+    print(f"ir_test.log age: {int(ir_test_log_age)} sec")
+    if ir_test_log_age < 12*60*60:
+        print(f"copying ir_test.log")
+        shutil.copyfile('ir_test.log', os.path.join(toplevel_logdir, 'ir_test.log'))
+    else:
+        print(f"NOT copying ir_test.log, because it is too old")
 
 print(f'\nlogdir = {toplevel_logdir}')
